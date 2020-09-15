a2eb6de4c8a614d01cf987e761beb498c09aae60
Sashank Thupukari <s@thupukari.com>
2020-09-15 14:10:52 -0400

    up

--- team_a_repository/team_a_repository/repo.py
+++ team_a_repository/team_a_repository/repo.py
@@ -1,8 +1,9 @@
 from dagster import repository
 
 from .pipelines import my_pipeline
+from .schedules import my_daily_schedule
 
 
 @repository
 def my_repository():
-    return [my_pipeline]
+    return [my_pipeline, my_daily_schedule]

9e41fc005f18d3ed5fd6d080c951177ed8dc70e9
Sashank Thupukari <s@thupukari.com>
2020-09-15 00:18:33 -0400

    Up

--- grpc_entrypoint.sh
+++ grpc_entrypoint.sh
@@ -2,4 +2,4 @@
 export DAGSTER_HOME=/opt/dagster/dagster_home
 
 # Launch Dagit as a service
-DAGSTER_HOME=/opt/dagster/dagster_home dagster api grpc --module-name dagster_demo --host 0.0.0.0 --port 4266
+DAGSTER_HOME=/opt/dagster/dagster_home dagster api grpc --module-name dagster_demo.repo --host 0.0.0.0 --port 4266
